5f0c663f14e1b1ed244e04f36b8167f2847139b5
goldirana <goldirana3210@gmail.com>
2025-01-27 16:08:59 +0530

    added data preprocessor

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = str(Path("/Users/goldyrana/mess/deep_learning/projects/Semantic-Search-Engine-for-Academic-Papers/config/config.yaml"))
-PARAMS_FILE_PATH = str(Path("params.yaml"))
+PARAMS_FILE_PATH = str(Path("/Users/goldyrana/mess/deep_learning/projects/Semantic-Search-Engine-for-Academic-Papers/params.yaml"))
